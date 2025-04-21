37a1a9dc929df7f0e99d6a97b15740b3689c7974
hungd19 <133303108+hungd19@users.noreply.github.com>
2025-04-21 11:55:21 +0700

    Create site.config.ts

---
+++ site.config.ts
@@ -0,0 +1,18 @@
+{
+  "network": "testnet",
+  "owner": "0xca1b913b901c9f309713b542699441f591674fa2e37e60098ae1b58b25ff8de4",
+  "site_name": "my-walrus-site",
+  "metadata": {
+    "link": "https://myproject.xyz",
+    "image_url": "https://myproject.xyz/preview.png",
+    "name": "My Project",
+    "description": "A decentralized web app deployed on Walrus.",
+    "project_url": "https://github.com/my-org/my-walrus-site",
+    "creator": "my-org"
+  },
+  "epochs": 30,
+  "path": "./dist",
+  "gas_budget": 100000000,
+  "write_retry_limit": 3,
+  "object_id": ""
+}
